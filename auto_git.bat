476c7640f91b79604eb7f921c5bc20c5781f737e
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-05-04 18:18:19 +0900

    auto commit

--- auto_git.bat
+++ auto_git.bat
@@ -3,7 +3,8 @@ git init
 git config --global user.name "Taishiro Kitahara"
 git config --global user.email "ktaishiro1227@gmail.com"
 git clone https://github.com/galileo15640215/atcoder C:\Users\ktais\github\atcoder\atcoder
-xcopy /d C:\Users\ktais\desktop\at C:\Users\ktais\github\atcoder
+xcopy /d C:\Users\ktais\desktop\at C:\Users\ktais\github\atcoder\atcoder
+cd atcoder
 git add --all
 git commit -m "auto commit"
 git push
