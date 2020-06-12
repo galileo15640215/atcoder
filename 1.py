5dd7f1b92ce007949d02f8e570812a3da8eb2c45
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-06-12 22:13:14 +0900

    auto commit

--- 1.py
+++ 1.py
@@ -1,4 +1,13 @@
-a = int(input())
-b = int(input())
-h = int(input())
-print(int((a+b)/2*h))
+n = int(input())
+a = [list(map(int, input().split())) for i in range(n-1)]
+dic = {}
+for i in range(n-1):
+    dic[a[i][0]] = []
+    dic[a[i][1]] = []
+for i in range(n-1):
+    dic[a[i][0]].append([a[i][1], a[i][2])
+    dic[a[i][1]].append([a[i][0], a[i][2])
+st = []
+st.append(dic[1])
+while st:
+    st.pop(-1)
