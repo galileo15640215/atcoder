57fdf9072623755fef0cac1faed2b8b4f4072654
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-06-12 21:06:20 +0900

    auto commit

--- 2.py
+++ 2.py
@@ -1,17 +1,6 @@
 import math
-from decimal import *
-a, b, c, d = map(int, input().split())
-n = int(input())
-t = [list(map(str, input().split())) for i in range(n)]
-su = 0
-for i in range(n):
-    res = b
-    p, q = t[i][0], int(t[i][1])
-    if q > a:
-        res += math.ceil((q-a)/c)*d
-    if p[0] == '0':
-        pass
-    elif int(p[0:2]) >= 22:
-        res = Decimal(str(res)) * Decimal("1.2")
-    su += res
-print(int(su))
+t = int(input())
+for i in range(t):
+    n, x, m = map(int, input().split())
+    l = [list(map(int, input().split())) for j in range(n)]
+    
