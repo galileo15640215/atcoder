90ffc34e5ddd447ab9430798509c9faa8c836c2d
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-07-03 21:51:41 +0900

    auto commit

--- abc022a.py
+++ abc022a.py
@@ -2,6 +2,8 @@ n, s, t = map(int, input().split())
 w = int(input())
 a = [int(input()) for i in range(n-1)]
 cnt = 0
+if s <= w <= t:
+    cnt += 1
 for i in range(n-1):
     w += a[i]
     if s <= w <= t:
