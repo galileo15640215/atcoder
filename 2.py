0298db53a1e9f2f683f82fc18a32082ca65116fc
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-07-27 12:43:22 +0900

    auto commit

--- 2.py
+++ 2.py
@@ -1,13 +1,23 @@
-a, b, c = map(int, input().split())
-k = int(input())
-cnt = 0
-while b <= a:
-    b *= 2
-    cnt += 1
-while c <= b:
-    c *= 2
-    cnt += 1
-if cnt <= k:
-    print("Yes")
-else:
-    print("No")
+t = int(input())
+x = []
+y = []
+for m in range(t):
+    p = int(input())
+    q = input()
+    x.append(p)
+    y.append(q)
+for case in range(t):
+    n = x[case]
+    a = y[case]
+    A = 0
+    B = 0
+    for i in range(n):
+        if a[i] == 'A':
+            A += 1
+        elif a[i] == 'B':
+            B += 1
+    if abs(A-B) < 3:
+        ans = 'Y'
+    else:
+        ans = 'N'   
+    print("Case #"+str(case+1)+": "+ans)
