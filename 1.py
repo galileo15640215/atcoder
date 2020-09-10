5d7c497cc6405a7e2b9b0a2a4e121574964a56be
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-09-10 23:38:16 +0900

    auto commit

--- 1.py
+++ 1.py
@@ -1,12 +1,2 @@
-a, b, c, d = map(int, input().split())
-aa = [0 for i in range(101)]
-bb = [0 for i in range(101)]
-ans = 0
-for i in range(a, b):
-    aa[i] = 1
-for i in range(c, d):
-    bb[i] = 1
-for i in range(101):
-    if aa[i] == 1 and bb[i] == 1:
-        ans += 1
-print(ans)
+a, b, c = map(int, input().split())
+print(a*b*c//(10**9+7))
--- abc041b.py
+++ abc041b.py
@@ -1,8 +1,2 @@
-n = int(input())
-a = list(map(int, input().split()))
-b = []
-for i in range(n):
-    b.append([a[i], i+1])
-b.sort(reverse=True, key=lambda x: x[0])
-for i in range(n):
-    print(b[i][1])
+a, b, c = map(int, input().split())
+print(a*b*c%(10**9+7))
