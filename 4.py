5a7d977ba64269253f92bb4463b467cb28c9a27f
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-09-13 21:24:20 +0900

    auto commit

--- 4.py
+++ 4.py
@@ -1,4 +1,3 @@
-n, k = map(int, input().split())
-p = list(map(int, input().split()))
-c = list(map(int, input().split()))
-while
+n = int(input())
+a, b = map(int, input().split())
+a = list(map(int, input().split()))
