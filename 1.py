ddc8a2a549af2117ea1e25ff4b947a3178dba389
Taishiro Kitahara <ktaishiro1227@gmail.com>
2020-10-02 21:29:44 +0900

    auto commit

--- 1.py
+++ 1.py
@@ -1,6 +1,19 @@
-k = int(input())
-s = ''
-for i in range(k):
-    s += 'ACL'
-print(s)
+l = list(soubu_week_df['station_name'])
+lon = list(soubu_week_df['lon'])
+lat = list(soubu_week_df['lat'])
+for i in range(len(l)):
+  if l[i] == '大久保':
+    lon[i] = 139.69732
+    lat[i] = 35.700784
+soubu_week_df['lon'] = lon
+soubu_week_df['lat'] = lat
 
+l = list(soubu_holi_df['station_name'])
+lon = list(soubu_holi_df['lon'])
+lat = list(soubu_holi_df['lat'])
+for i in range(len(l)):
+  if l[i] == '大久保':
+    lon[i] = 139.69732
+    lat[i] = 35.700784
+soubu_holi_df['lon'] = lon
+soubu_holi_df['lat'] = lat
